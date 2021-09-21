795cea54f6008441fe69bd73b9d33d16cd923871
unknown <99067718@mydavinci.nl>
2021-09-21 10:57:54 +0200

    print('alle tijden van de dag')

---
+++ duizelig/AlleUrenVanDeDag.py
@@ -0,0 +1,12 @@
+
+ochtend = 1
+avond = 1
+
+
+while ochtend <= 12:
+    print(str(ochtend), " AM")
+    ochtend = ochtend + 1
+print("Het is nu middag")
+while avond <= 12:
+    print(str(avond)," PM")
+    avond = avond + 1

501a784616b174b9a533fad2d5381356d7e2b1d0
unknown <99067718@mydavinci.nl>
2021-09-21 11:41:20 +0200

    quit

--- duizelig/quit.py
+++ duizelig/quit.py
@@ -1,6 +1,9 @@
 exitcode = ""
+aantalherhalingen = 0
 print("welcome sir")
 while exitcode != "quit":
-    
+    aantalherhalingen = aantalherhalingen + 1
     exitcode = input("type 'quit' to exit the program. ")
     print("")
+    if exitcode != "quit":
+        print("Try again, ", aantalherhalingen, "failed attempts")

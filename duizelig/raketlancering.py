3306ef8d0169ac69320a8c45d3a57b8aecf3937d
unknown <99067718@mydavinci.nl>
2021-09-21 10:23:45 +0200

    RaketLancering

---
+++ duizelig/raketlancering.py
@@ -0,0 +1,10 @@
+import time
+secondsBeforeLaunch = 30
+while secondsBeforeLaunch >= 1:
+    print(secondsBeforeLaunch)
+    time.sleep(1)
+    secondsBeforeLaunch = secondsBeforeLaunch - 1
+print("LANCEREN!!!!")
+
+#Emiel 't lam
+#99067718
